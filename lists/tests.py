9f79ecfc4c5dd84fa79dbd38810b10198a803d21
Aamir Mirza <spookane112@gmail.com>
2021-02-09 13:06:48 -0500

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -8,13 +8,35 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
+        # TODO: Code smell, POST test is too long?
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_post_request(self):
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_redirects_after_post_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_can_save_a_post_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='Itemy 1')
+        Item.objects.create(text='Itemy 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('Itemy 1', response.content.decode())
+        self.assertIn('Itemy 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
