266ff6fef57aebf7ab8694b98e705758fcf3b404
Aamir Mirza <spookane112@gmail.com>
2021-03-18 15:56:30 -0400

    url, placeholder view, and first-cut templates for my_lists

--- functional_tests/test_my_list.py
+++ functional_tests/test_my_list.py
@@ -26,10 +26,50 @@ class MyListTest(FunctionalTest):
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         email = 'edith@example.com'
-        self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_out(email)
+        # self.browser.get(self.live_server_url)
+        # self.wait_to_be_logged_out(email)
 
         # Edith is a logged-in user
         self.create_pre_authenticated_session(email)
+        # self.browser.get(self.live_server_url)
+        # self.wait_to_be_logged_in(email)
         self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_in(email)
+        first_item = 'Reticulate splines'
+        self.add_list_item(first_item)
+        self.add_list_item('Immanentize eschaton')
+        first_list_url = self.browser.current_url
+
+        # She notices a "My lists" link, for the first time.
+        self.browser.find_element_by_link_text('My lists').click()
+
+        # She sees that her list is in there, named according to its
+        # first list item
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text(first_item)
+        )
+        self.browser.find_element_by_link_text(first_item).click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, first_list_url)
+        )
+
+        # She decides to start another list, just to see
+        self.browser.get(self.live_server_url)
+        new_list_item = 'Click cows'
+        self.add_list_item(new_list_item)
+        second_list_url = self.browser.current_url
+
+        # Under "my lists", her new list appears
+        self.browser.find_element_by_link_text('My lists').click()
+        self.wait_for(
+            self.browser.find_element_by_link_text(new_list_item)
+        )
+        self.browser.find_element_by_link_text(new_list_item).click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, second_list_url)
+        )
+        self.browser.find_element_by_link_text('Log out').click()
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_elements_by_link_text('My lists'), []
+            )
+        )
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -22,18 +22,17 @@ class NewVisitorTest(FunctionalTest):
 
         # She types "buy peacock feathers" into a text box (Edith'' hobby
         # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
+        self.add_list_item('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+        self.add_list_item('Use peacock feathers to make a fly')
         inputbox = self.get_item_input_box()
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on ehr list.
 
